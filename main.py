e56a9526620dee5f87083fd8a9790e062f92bc8d
Peter Holderreith <lhw539@compg005.hpc.in.bmrc.ox.ac.uk>
2021-01-05 09:19:27 +0000

    Logout update

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ if torch.cuda.is_available():
 else:
     DEVICE = torch.device("cpu")
     print("Running on the CPU")
+for i in range(10000):
+	x=torch.randn((200,20))
+	y=x**2+torch.randn((200,20))
+	x=x.to(DEVICE)
+	y=y.to(DEVICE)
+	diff=torch.norm(y-x,dim=1)
+	print(torch.mean(diff))
 
 
