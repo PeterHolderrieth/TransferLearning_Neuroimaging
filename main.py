68255b21ae4427c6a8d2cd646a01f95a8da11bde
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-01-06 15:43:25 +0100

    Logout update

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import torch 
+from dp_model.model_files.sfcn import SFCN
+from dp_model import dp_loss as dpl
+from dp_model import dp_utils as dpu
+import torch
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda:0")  
     print("Running on the GPU")
 else:
     DEVICE = torch.device("cpu")
     print("Running on the CPU")
-for i in range(10000):
-	x=torch.randn((200,20))
-	y=x**2+torch.randn((200,20))
-	x=x.to(DEVICE)
-	y=y.to(DEVICE)
-	diff=torch.norm(y-x,dim=1)
-	print(torch.mean(diff))
+'''
+
 
 
