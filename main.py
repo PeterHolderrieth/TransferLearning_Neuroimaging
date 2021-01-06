a44b13e03edea6dc21102c9563bb506744e17abd
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-01-06 15:55:33 +0100

    Logout update

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
+
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda:0")  
     print("Running on the GPU")
 else:
     DEVICE = torch.device("cpu")
     print("Running on the CPU")
-'''
 
 
-
+# Example
+model = SFCN()
+model = torch.nn.DataParallel(model)
+fp_ = './pre_trained_models/brain_age/run_20190719_00_epoch_best_mae.p'
+model.load_state_dict(torch.load(fp_,map_location=DEVICE))
