f8b5b14f4b4c780a215f057ab4fd229838ae3dc8
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-01-09 23:28:21 +0100

    Logout update

--- dp_model/dp_loss.py
+++ dp_model/dp_loss.py
@@ -1,14 +1,20 @@
 import torch.nn as nn
+import torch 
 
 def my_KLDivLoss(x, y):
-    """Returns K-L Divergence loss
+    """
+    Input: 
+        x  - torch.tensor - shape (n, m) - log-probabilities!
+        y  - torch.tensor - shape (n, m) - probabilities
+    Output:
+        loss - float
+    Returns KL-Divergence KL(y||x)
     Different from the default PyTorch nn.KLDivLoss in that
     a) the result is averaged by the 0th dimension (Batch size)
-    b) the y distribution is added with a small value (1e-16) to prevent log(0) problem
+    b) the y distribution is added with a small value (1e-16) to ensure
+    numerical stability ("log(0)-problem")
     """
-    loss_func = nn.KLDivLoss(reduction='sum')
+    loss_func = nn.KLDivLoss(reduction='batchmean')
     y += 1e-16
-    n = y.shape[0]
-    loss = loss_func(x, y) / n
-    #print(loss)
+    loss = loss_func(x, y) 
     return loss
