4ce51d458277a17aa1515a620d875d3e7959a0e5
Peter Holderreith <lhw539@rescomp1.hpc.in.bmrc.ox.ac.uk>
2021-01-29 16:38:48 +0000

    Logout update

--- elastic.py
+++ elastic.py
@@ -1,12 +1,15 @@
+import numpy as np
 from sklearn.decomposition import PCA, IncrementalPCA
 from sklearn.preprocessing import scale
 from sklearn.linear_model import ElasticNet
-import numpy as np
-import data.oasis.load_oasis3 as load_oasis
 import argparse
 import sys 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
+
+#Own files:
+import data.oasis.load_oasis3 as load_oasis
+
 
 # Construct the argument parser
 ap = argparse.ArgumentParser()
@@ -17,36 +20,63 @@ ap.set_defaults(
     l1rat=0.5,
     reg=1.)
 
-#Debugging? Then use small data set:
 ap.add_argument("-deb", "--DEBUG", type=str, required=True,help="'debug' or 'full'.")
 ap.add_argument("-batch", "--BATCH", type=int, required=True,help="Batch size.")
 ap.add_argument("-ncomp", "--N_COMP", type=int, required=False,help="Number of principal components.")
 ap.add_argument("-l1rat", "--L1RAT", type=float, required=True,help="Ratio of L1 loss (compared to L2).")
-ap.add_argument("-reg", "--REG", type=float, required=True,help="Scaling of elastic regularizer.")
+ap.add_argument("-reg", "--REG", type=float, required=True,help="Scaling of ElasticNet regularizer term.")
 
-
-#Arguments for tracking:
+#Get arguments:
 ARGS = vars(ap.parse_args())
 print(ARGS)
 
+#A batch version of PCA:
 def batch_fit_pca(data_loader,n_components):
+    '''
+    Inputs:
+        data_loader - pytorch data loader giving data in batches
+        n_components - int  - number of principal components to use
+    Output:
+        pca - sklearn.decomposition.PCA - pca function fitted to data from data_loader
+    '''
     batch_size=data_loader.batch_size
+    
     pca=IncrementalPCA(n_components=n_components,batch_size=batch_size)
+
     for batch_idx, (X,Y) in enumerate(data_loader):
-        X=X.reshape(ARGS['BATCH'],np.prod(X.shape[1:]))
+        X=X.reshape(batch_size,-1)
         pca.partial_fit(X)
     return(pca)
 
+#A function transforming data from a data loader via PCA into a data matrix:
 def batch_trans_pca(pca,data_loader):
+    '''
+    Inputs:
+        pca - sklearn.decomposition.PCA - pca function 
+        data_loader - pytorch data loader giving data in batches
+    Output:
+        np.array - shape (k,n_components) where n_components are given by the number of components from pca
+    '''
+    #Init empty lists:
     trans_list=[]
     age_list=[]
-    for batch_idx, (X,Y) in enumerate(train_loader):
-        X=X.reshape(ARGS['BATCH'],np.prod(X.shape[1:]))
+
+    batch_size=data_loader.batch_size
+
+    for batch_idx, (X,Y) in enumerate(data_loader):
+
+        #Reshape inputs:
+        X=X.reshape(batch_size,-1)
+        Y=Y.flatten()
+        
+        #Add transformed X and non-transformed Y:
         trans_list.append(pca.transform(X))
-        age_list.append(Y.flatten())
+        age_list.append(Y)
+
     #Concatenate:
     data_trans=np.concatenate(trans_list,axis=0)
     label=np.concatenate(age_list,axis=0)
+
     return(data_trans,label)
 
 
@@ -74,42 +104,23 @@ data_trans,label=batch_trans_pca(pca,train_loader)
 eln=ElasticNet(alpha=ARGS['REG'],l1_ratio=ARGS['L1RAT'])
 eln.fit(data_trans,label)
 
-
-#Reshape validation data set:
+#Get validation data set:
 _,val_loader=load_oasis.give_oasis_data('val',batch_size=ARGS['BATCH'],debug=DEBUG,shuffle=False)
+
+#Get transformed validation data:
 val_data_trans,val_label=batch_trans_pca(pca,val_loader)
 
-#Predict:
+#Predict on validation set:
 Predic=eln.predict(val_data_trans)
 
 #Get mean absolute error (mae):
-mae=np.mean(np.abs(Predic-val_label))
-mae_stupid=np.mean(np.abs(val_label-np.mean(label)))
-mae_val=np.mean(np.abs(val_label-np.mean(val_label)))
+mae=(Predic-val_label).abs().mean()
+
+#Get "stupid" baselines:
+mae_stupid=(val_label-np.mean(label)).abs().mean()
+mae_val=(val_label-np.mean(val_label)).abs().mean()
 
 #Print results:
-print("Mean absolute: ",mae)
-print("Mean absolute deviance of stupid guess: ", mae_stupid)
-print("Mean absolute deviance of validation set: ", mae_val)
-
-
-
-
-
-'''
-def get_oasis_data_matrix(data_type='train',n_samples=10):
-    scan_list=[]
-    age_list=[]
-    data_set,_=load_oasis.give_oasis_data(data_type,preprocessing='min')
-    ind=np.random.permutation(np.arange(0,data_set._len))[:n_samples]
-    for it in range(n_samples): 
-        new_item=data_set.get_data(ind[it])
-        x_new=new_item[0].flatten()
-        scan_list.append(x_new)
-        y_new=new_item[1]
-        age_list.append(y_new)
-    X=np.stack(scan_list)
-    Y=np.array(age_list).flatten()
-    X=scale(X)
-    return(X,Y)
-'''
+print("MAE of ElasticNet: ",mae)
+print("MAE on valid when train mean is predicted: ", mae_stupid)
+print("MAE on valid when valid mean is predicted: ", mae_val)
