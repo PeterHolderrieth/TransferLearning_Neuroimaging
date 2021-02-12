a70cad019210a279c0a60468219b77043e75766c
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-02-12 15:15:01 +0000

    Login update

--- tensor_board_test.py
+++ tensor_board_test.py
@@ -6,6 +6,7 @@ epoch_list=torch.rand(6)
 loss_list=torch.rand(6)
 for it in range(len(epoch_list)):
     print(epoch_list[it])
+    print(loss_list[it])
     writer.add_scalar("Loss/train", loss_list[it].item(), epoch_list[it].item())
 
 writer.close()
