971033b6ab99183889a491d77690fa79311ac214
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-03-01 18:16:23 +0000

    Update

--- data/ixi/split_ixi.py
+++ data/ixi/split_ixi.py
@@ -123,6 +123,19 @@ print_summary(df_val["Age"],'Val Age')
 print_summary(df_test["Age"],'Test Age')
 
 
+#Print summary to see that the split is sex-matched:
+print_summary(df_ixi["Sex"],'Sex')
+print_summary(df_train["Sex"],'Train Sex')
+print_summary(df_val["Sex"],'Val Sex')
+print_summary(df_test["Sex"],'Test Sex')
+
+#Print summary to see that the split is age-matched:
+print_summary(df_ixi["IsNC"],'IsNC')
+print_summary(df_train["IsNC"],'IsNC')
+print_summary(df_val["IsNC"],'IsNC')
+print_summary(df_test["IsNC"],'IsNC')
+
+
 #Save train, validation and test files:
 df_train.to_csv(folder+'ixi_train.csv')
 df_val.to_csv(folder+'ixi_val.csv')
