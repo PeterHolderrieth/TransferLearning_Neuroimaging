219b55c40644bf4b68c0bc203843fe098347d5ff
Peter Holderreith <lhw539@rescomp1.hpc.in.bmrc.ox.ac.uk>
2021-03-04 12:55:54 +0000

    Login update

--- create_feature_matrix.py
+++ create_feature_matrix.py
@@ -9,7 +9,7 @@ import pandas as pd
 #Look up freesurfer.
 
 debug=False
-method='pca'
+methods=['pca','tsne','isomap']
 
 train_dataset,_=give_oasis_data('train', batch_size=5,
                                         num_workers=4,
@@ -21,49 +21,50 @@ train_dataset,_=give_oasis_data('train', batch_size=5,
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Number of GPUs: ", torch.cuda.device_count())
 
-for run in range(4):
-    model=give_pretrained_sfcn(str(run), "age")
-    model.eval()
-    model.module.train_nothing() 
-    model=model.to(device)
+for method in methods:
+    for run in range(4):
+        model=give_pretrained_sfcn(str(run), "age")
+        model.eval()
+        model.module.train_nothing() 
+        model=model.to(device)
 
-    n_samples=train_dataset._len
-    n_final_channels=model.module.channel_number[-1]
-    feat_matrix=np.zeros(shape=(n_samples,n_final_channels))
+        n_samples=train_dataset._len
+        n_final_channels=model.module.channel_number[-1]
+        feat_matrix=np.zeros(shape=(n_samples,n_final_channels))
 
-    for idx in range(n_samples):
-        print("Element: ", idx)
-        data,_=train_dataset.get_data(idx)
-        data=torch.tensor(data[np.newaxis,...],dtype=torch.float32,requires_grad=False)
-        data=data.to(device)
-        output=model.module.feature_extractor(data)
-        output=model.module.classifier.average_pool(output)
-        feat_matrix[idx]=output.flatten().detach().cpu().numpy()
+        for idx in range(n_samples):
+            print("Element: ", idx)
+            data,_=train_dataset.get_data(idx)
+            data=torch.tensor(data[np.newaxis,...],dtype=torch.float32,requires_grad=False)
+            data=data.to(device)
+            output=model.module.feature_extractor(data)
+            output=model.module.classifier.average_pool(output)
+            feat_matrix[idx]=output.flatten().detach().cpu().numpy()
 
-    fp=train_dataset.file_list
+        fp=train_dataset.file_list
 
-    if method=='tsne':
-        tsne=TSNE(n_components=2, init='pca')
-        print("Compute tSNE.")
-        trans_feat_matrix=tsne.fit_transform(feat_matrix)
-        print("Finished computing tSNE.")
-    
-    elif method=='isomap':
-        isomap=Isomap()
-        print("Compute isomap.")
-        trans_feat_matrix=isomap.fit_transform(feat_matrix)
-        print("Finished computing isomap.")
-    
-    elif method=='pca':
-        pca=PCA(n_components=2)
-        print("Compute pca.")
-        trans_feat_matrix=pca.fit_transform(feat_matrix)
-        print("Finished computing pca.")
+        if method=='tsne':
+            tsne=TSNE(n_components=2, init='pca')
+            print("Compute tSNE.")
+            trans_feat_matrix=tsne.fit_transform(feat_matrix)
+            print("Finished computing tSNE.")
+        
+        elif method=='isomap':
+            isomap=Isomap()
+            print("Compute isomap.")
+            trans_feat_matrix=isomap.fit_transform(feat_matrix)
+            print("Finished computing isomap.")
+        
+        elif method=='pca':
+            pca=PCA(n_components=2)
+            print("Compute pca.")
+            trans_feat_matrix=pca.fit_transform(feat_matrix)
+            print("Finished computing pca.")
 
-    df=pd.DataFrame(trans_feat_matrix,columns=['tSNE1','tSNE2'])
-    df['fp']=fp
-    df['age']=[float(label[0]) for label in train_dataset.label_list]
+        df=pd.DataFrame(trans_feat_matrix,columns=['tSNE1','tSNE2'])
+        df['fp']=fp
+        df['age']=[float(label[0]) for label in train_dataset.label_list]
 
-    folder="visualization/data/"
-    filename="oasis_run_"+str(run)+'_'+method+'.csv'
-    df.to_csv(folder+filename)
+        folder="visualization/data/"
+        filename="oasis_run_"+str(run)+'_'+method+'.csv'
+        df.to_csv(folder+filename)
