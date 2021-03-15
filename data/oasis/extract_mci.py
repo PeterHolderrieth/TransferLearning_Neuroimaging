82b7933934c1cc8111fd745f6d1af61754f1f18a
Peter Holderreith <lhw539@rescomp1.hpc.in.bmrc.ox.ac.uk>
2021-03-15 17:27:08 +0000

    Login update

--- data/oasis/extract_mci.py
+++ data/oasis/extract_mci.py
@@ -1,14 +1,29 @@
 
 import pandas as pd 
 import numpy as np
+
+#Load info of data:
 DIR = '/gpfs3/well/win-fmrib-analysis/users/lhw539/oasis3/oasis3_info/'
-fp_ = DIR+'session_info_clinical.csv'    
-df_session = pd.read_csv(fp_)
+fp_ = DIR+'session_info.csv'#_clinical.csv'    
+# df_session = pd.read_csv(fp_)
+# print(df_session.head())
+# print(df_session.shape)
 
-df_mci=df_session.loc[(df_session["cdr"]>0)&(df_session["cdr"]<2)]
+fp_clinical = DIR+'session_info_clinical.csv'    
+df_clinical = pd.read_csv(fp_clinical)
+# print(df_clinical.head())
+# print(df_clinical.shape)
+
+# #df_mci=df_session.loc[(df_session["cdr"]>0)&(df_session["cdr"]<2)]
+# subject_ids,counts=np.unique(df_session["Subject"],return_counts=True)
+# print("Number of subjects with two or more sessions:", counts[counts>1].shape[0])
+
+# subject_ids,counts=np.unique(df_clinical["Subject"],return_counts=True)
+# print("Number of subjects with two or more sessions:", counts[counts>1].shape[0])
+
+df_mci=df_clinical[(df_clinical["cdr"]>0)&(df_clinical["cdr"]<2)]
 subject_ids,counts=np.unique(df_mci["Subject"],return_counts=True)
-print("Number of subjects with two or more sessions:", counts[counts>=].shape[0])
-
+print("Number of subjects with two or more sessions and CDR>0:", counts[counts>1].shape[0])
 
 #print(df_session[["Subject","Age","ageAtEntry","cdr"]].head())
 
