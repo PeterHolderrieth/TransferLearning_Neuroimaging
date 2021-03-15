ed0e1cee18fa0085781af183e3d79fc4825b6a96
PeterHolderrieth <peter.holderrieth@new.ox.ac.uk>
2021-03-15 15:32:34 +0000

    Update

---
+++ experiments/abide/age/scratch/exp_4_abide_age_scratch/exp_4_abide_age_scratch20210315_1525.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -wd /users/win-fmrib-analysis/lhw539/TransferLearning_Neuroimaging/
+#$ -P win.prjc
+#$ -q gpu8.q
+#$ -j y #Error and output file are merged to output file
+#$ -l gpu=2
+#$ -pe shmem 2 #Should be the same as the number of GPUs 
+#$ -l gputype=p100
+#Save file to:
+# Log locations which are relative to the current                                                                                                                                                                  # working directory of the submission
+#$ -o experiments/abide/age/scratch/exp_4_abide_age_scratch/exp_4_abide_age_scratch20210315_1525.log
+
+echo "------------------------------------------------"
+echo "Job ID: $JOB_ID"
+echo "Run on host: "`hostname`
+echo "Operating system: "`uname -s`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo "------------------------------------------------"
+
+#Load Python module:
+module load Python/3.7.4-GCCcore-8.3.0
+
+#Activate the correct python environment:
+source ~/python/ccpu_py_tlneuro
+
+python ~/TransferLearning_Neuroimaging/main.py -deb full -con experiments/abide/age/scratch/exp_4_abide_age_scratch/exp_4_abide_age_scratch20210315_1525.json
+
+
+echo "------------------------------------------------"
+echo "Finished at: "`date`
+echo "------------------------------------------------" 
+
