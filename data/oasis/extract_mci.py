0cbb01dba0363032e92b5a295fac68b0f277d6e8
Peter Holderreith <lhw539@rescomp1.hpc.in.bmrc.ox.ac.uk>
2021-03-15 16:58:21 +0000

    Login update

--- data/oasis/extract_mci.py
+++ data/oasis/extract_mci.py
@@ -5,9 +5,17 @@ DIR = '/gpfs3/well/win-fmrib-analysis/users/lhw539/oasis3/oasis3_info/'
 fp_ = DIR+'session_info_clinical.csv'    
 df_session = pd.read_csv(fp_)
 
-print("Shape of data frame: ", df_session.shape)
-print("Variables: ", list(df_session.columns))
+df_mci=df_session.loc[(df_session["cdr"]>0)&(df_session["cdr"]<2)]
+subject_ids,counts=np.unique(df_mci["Subject"],return_counts=True)
+print("Number of subjects with two or more sessions:", counts[counts>=].shape[0])
 
+
+#print(df_session[["Subject","Age","ageAtEntry","cdr"]].head())
+
+#print("Shape of data frame: ", df_session.shape)
+#print("Variables: ", list(df_session.columns))
+
+'''
 #Get all possible CDR scores:
 cdr_val=np.unique(df_session["cdr"].to_numpy()).astype(float)
 print("Possible CDR scores: ", cdr_val)
@@ -19,4 +27,4 @@ for it in range(cdr_val.size):
     cdr=cdr_val[it]
     n_subjects=df_session.loc[df_session["cdr"]==cdr].shape[0]
     print("%4d sessions with CDR score of %.1f"%(n_subjects,cdr))
-
+'''
