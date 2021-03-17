958b6cd71a7dab81410e672a0404eedb46e9b891
Peter Holderreith <lhw539@rescomp1.hpc.in.bmrc.ox.ac.uk>
2021-03-17 15:35:39 +0000

    Login update

---
+++ experiments/oasis/age/elastic/final_3_oasis_age_elastic/final_3_oasis_age_elastic20210317_1501.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -wd /users/win-fmrib-analysis/lhw539/TransferLearning_Neuroimaging/
+#$ -P win.prjc
+#$ -q win000
+#$ -j y #Error and output file are merged to output file
+#$ -l gpu=0
+#$ -pe shmem 0 #Should be the same as the number of GPUs 
+#$ -l gputype=p100
+#Save file to:
+# Log locations which are relative to the current                                                                                                                                                                  # working directory of the submission
+#$ -o experiments/oasis/age/elastic/final_3_oasis_age_elastic/final_3_oasis_age_elastic20210317_1501.log
+
+echo "------------------------------------------------"
+echo "Job ID: $JOB_ID"
+echo "Run on host: "`hostname`
+echo "Operating system: "`uname -s`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo "------------------------------------------------"
+
+#Load Python module:
+module load Python/3.7.4-GCCcore-8.3.0
+
+#Activate the correct python environment:
+source ~/python/ccpu_py_tlneuro
+
+python ~/TransferLearning_Neuroimaging/main.py -deb full -con experiments/oasis/age/elastic/final_3_oasis_age_elastic/final_3_oasis_age_elastic20210317_1501.json
+
+
+echo "------------------------------------------------"
+echo "Finished at: "`date`
+echo "------------------------------------------------" 
+
---
+++ experiments/oasis/age/scratch/old/M_scratch_baseline_07022021.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#$ -wd /users/win-fmrib-analysis/lhw539/TransferLearning_Neuroimaging/experiments/brain_age/scratch_baseline/
+#$ -P win.prjc
+#$ -q gpu8.q
+#$ -j y #Error and output file are merged to output file
+#$ -l gpu=2
+#$ -pe shmem 2 #Should be the same as the number of GPUs 
+#$ -l gputype=p100
+#Save file to:
+# Log locations which are relative to the current                                                                                                                                                                  # working directory of the submission
+#$ -o results/M_scratch_baseline.log
+
+echo "------------------------------------------------"
+echo "Job ID: $JOB_ID"
+echo "Run on host: "`hostname`
+echo "Operating system: "`uname -s`
+echo "Username: "`whoami`
+echo "Started at: "`date`
+echo "------------------------------------------------"
+
+#Load Python module:
+module load Python/3.7.4-GCCcore-8.3.0
+
+#Activate the correct python environment:
+source ~/python/ccpu_py_tlneuro
+
+debug=full
+
+python ~/TransferLearning_Neuroimaging/train.py \
+-deb $debug \
+-train fresh \
+-loss kl \
+-batch 8 \
+-n_work 4 \
+-lr 1e-2 \
+-gamma 0.3 \
+-epochs 1000 \
+-pat 60 \
+-wdec 1e-3 \
+-mom 0.9
+
+echo "------------------------------------------------"
+echo "Finished at: "`date`
+echo "------------------------------------------------"
